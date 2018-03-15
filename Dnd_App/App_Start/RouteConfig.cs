8c50ab9434d9c2a926fcc965b1ac72dcd41e8031
Sergio Fernandez <sfernandez11@alumnos.utalca.cl>
2018-03-15 00:58:11 -0300

    Fix routes

--- Dnd_App/App_Start/RouteConfig.cs
+++ Dnd_App/App_Start/RouteConfig.cs
@@ -13,6 +13,25 @@ namespace Dnd_App
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            
+            routes.MapRoute(
+                name: "Panel",
+                url: "Panel",
+                defaults: new { controller = "User", action = "Panel"}
+            );
+            
+            routes.MapRoute(
+                name: "Login",
+                url: "Login",
+                defaults: new { controller = "User", action = "Login" }
+            );
+
+            routes.MapRoute(
+                name: "Register",
+                url: "Register",
+                defaults: new { controller = "User", action = "Register" }
+            );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
