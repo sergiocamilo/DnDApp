0387e680b4cf6d5b5cac6cf8d8e1a7cbc852a00c
Sergio Fernandez <sfernandez11@alumnos.utalca.cl>
2018-04-28 23:22:07 -0300

    Add Error Page

---
+++ Dnd_App/Controllers/ErrorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dnd_App.Controllers
+{
+    public class ErrorController : Controller
+    {
+
+        public ActionResult Error(int id)
+        {
+            Response.StatusCode = id;
+
+            return View();
+        }
+
+    }
+}
